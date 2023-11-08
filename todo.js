ca644fbdfa9770ccb332fc6462f6847f471653d1
Larry <user@Users-MacBook-Pro.local>
2023-11-08 13:12:39 -0600

    first commit

---
+++ todo.js
@@ -0,0 +1,24 @@
+/*
+Create a task
+Delete All Tasks
+Mark a task as completed
+Show all tasks
+*/
+
+const todoList = [];
+
+function showAllTasks() {
+ 
+}
+
+function addTask() {
+ 
+}
+
+function deleteAllTasks() {
+  
+}
+
+function markTaskAsCompleted(index) {
+  
+}
